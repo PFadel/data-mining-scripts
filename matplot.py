679052d416e5da840aaf2e6f41d5d715e211d56b
Pedro Fadel <pfadel@stone.com.br>
2020-03-24 09:28:29 -0300

    :zap: creates total_hours column

--- matplot.py
+++ matplot.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 data = pd.read_csv('all_participants.csv', sep=',', index_col =0)
 
+total_hours = data["facebook_hours"] + data["twitter_hours"]
 
-data.plot.hist(by='facebook_hours')
+data["total_hours"] = total_hours
+
+data.plot.hist(by='total_hours')
 
 plt.show()
